2bad322f159cfb24390d03e9e38051ec9c0aa751
alexander diaz <alexanderjdiaz89@gmail.com>
2020-01-02 19:42:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -52,12 +52,12 @@ row_index = 0
 while row_index < src.count do
  element_index = 0
  while element_index < src[row_index].count do
-   if src[row_index][element_index][0] % src[row_index][element_index][1] == 0
+   if src[row_index][element_index] % src[row_index][element_index] == 0
    binding.pry
    total += src[row_index][element_index]
    element_index += 1
  else 
-   if src[row_index][element_index][0] % src[row_index][element_index][1] == 1
+   if src[row_index][element_index] % src[row_index][element_index] == 1
      arr << src[row_index][element_index]
      element_index += 1
  end 
