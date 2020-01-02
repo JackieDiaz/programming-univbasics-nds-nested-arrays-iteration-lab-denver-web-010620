5f5122a3d04eee5c9c8844ff25ce78d90fd607ae
alexander diaz <alexanderjdiaz89@gmail.com>
2020-01-02 19:42:50 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -53,7 +53,6 @@ while row_index < src.count do
  element_index = 0
  while element_index < src[row_index].count do
    if src[row_index][element_index] % src[row_index][element_index] == 0
-   binding.pry
    total += src[row_index][element_index]
    element_index += 1
  else 
